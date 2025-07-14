6679a05422128b43fecb0abcee34d86890420351
unknown1-name <hatunknown3@gmail.com>
2025-07-14 12:54:04 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ app.use('/api/admin', adminRoutes);
 
 mongoose.connect(config.mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
   })
   .catch(err => console.log(err));

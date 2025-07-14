6e3c76cc2751ea1c2049262f4e8f77ee3d3434e2
unknown1-name <hatunknown3@gmail.com>
2025-07-14 17:49:53 +0300

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,6 +3,7 @@ const messageSchema = new mongoose.Schema({
   from: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   to: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   content: { type: String, required: true },
+  image: { type: String, default: null },
   createdAt: { type: Date, default: Date.now },
   replyTo: { type: mongoose.Schema.Types.ObjectId, ref: 'Message', default: null }
 });
